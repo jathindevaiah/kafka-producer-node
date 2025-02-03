8314272c078c897fa4413c9a99313d57bcef811e
Ramesh <devaiah.ramesh@accenture.com>
2025-02-02 20:27:03 -0500

    Adding schema for message production

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
-import { Kafka } from 'kafkajs';
-import dotenv from 'dotenv';
+import { Kafka } from "kafkajs";
+import dotenv from "dotenv";
+import { SchemaRegistry } from "@kafkajs/confluent-schema-registry";
+import axios from "axios";
 
 dotenv.config();
 
@@ -9,7 +11,7 @@ const kafka = new Kafka({
   brokers: [process.env.KAFKA_BROKER], // Replace with your Confluent Cloud broker(s)
   ssl: true,
   sasl: {
-    mechanism: 'plain',
+    mechanism: "plain",
     username: process.env.KAFKA_API_KEY, // Replace with your API key
     password: process.env.KAFKA_API_SECRET, // Replace with your API secret
   },
@@ -18,18 +20,52 @@ const kafka = new Kafka({
 // Create producer
 const producer = kafka.producer();
 
+// Confluent Schema Registry URL
+const registry = new SchemaRegistry({
+  host: process.env.KAFKA_SR_URL, // Schema Registry URL
+  auth: {
+    username: process.env.KAFKA_SR_API_KEY, // API key
+    password: process.env.KAFKA_SR_API_SECRET, // API secret
+  },
+});
+
+//Test SIC message matching the schema
+const sicMessage = {};
+
+//Test SII message matching the schema
+const siiMessage = {};
+
 const run = async () => {
   await producer.connect();
 
   try {
+    // Fetch the latest version of the schema (it should already be registered)
+    // const schemaInfo = await axios.get(`${process.env.KAFKA_SR_URL}/subjects/${process.env.KAFKA_SR_SCHEMA_SUBJECT}/versions/latest`, {
+    //   auth: {
+    //     username: process.env.KAFKA_SR_API_KEY,
+    //     password: process.env.KAFKA_SR_API_SECRET,
+    //   },
+    // });
+    // const schema = schemaInfo.data.schema;
+
+    // Serialize the message using the schema (Avro)
+    const serializedMessage = await registry.encode(process.env.KAFKA_SR_SCHEMA_ID, siiMessage);
+    console.log("serializedMessage", serializedMessage);
+
     // Send a message to a Kafka topic
     await producer.send({
-      topic: 'test-topic', // Replace with your topic name
-      messages: [{ value: 'Hello Kafka from Node.js!' }],
+      topic: process.env.KAFKA_TOPIC,
+      // messages: [{ value: "Hello Kafka from Node.js!" }],
+      messages: [
+        {
+          key: "user-key3",
+          value: serializedMessage, // Avro-encoded message
+        },
+      ],
     });
-    console.log('Message sent successfully');
+    console.log("Message sent successfully");
   } catch (error) {
-    console.error('Error producing message:', error);
+    console.error("Error producing message:", error);
   } finally {
     await producer.disconnect();
   }
